59d64dbcaa541cf617e8bbf33a6dc5542c3eebb1
Dreve Cosmin <cdreve@gmail.com>
2016-08-25 19:17:44 +0300

    For exercise1

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -2,16 +2,27 @@
 
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./controllers/routes');
+
+const addHeaderShenanigans = require('./middleware/add-header-shenanigans');
+const removeHeaderPoweredBy = require('./middleware/remove-header-powered-by');
 
 let app = express();
 
 // Configure view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+// app.set('x-powered-by', false); -- Better solution
+// app.disable('x-powered-by'); -- Even Better solution
 
 // Configure middleware
+app.use(morgan('combined'));
+
+app.use(addHeaderShenanigans);
+app.use(removeHeaderPoweredBy);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static file serving happens everywhere but in production
